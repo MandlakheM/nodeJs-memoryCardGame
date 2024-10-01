99708e481405b8fdc6c9ee059f36f78cf60ba406
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-10-01 10:16:37 +0200

    i have managed to create a node express server that sends back a response of the index.html file. i have added an endpoint that will be responsible for returning back the shuffled cards, for the shuffling of cards i came across an efficient way to shuffle my array of cards its called the Fisher-Yates shuffle algorithm hopefully it behaves as it should but to test it out i am moving on to creating the a script file and adding the logic of the game

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require("express");
-const path = require("node:fs");
-const fs = require("node:fs");
+const path = require("path");
+const fs = require("fs");
 
-const app = express;
+const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/cards", (req, res) => {
+  const cards = JSON.parse(fs.readFileSync("./data/cards.json", "utf8"));
+  const shuffledCards = shuffleCards([...cards, ...cards]);
+  res.json(shuffledCards);
+});
+
+//Fisher-Yates shuffle algorithm
+function shuffleCards(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
